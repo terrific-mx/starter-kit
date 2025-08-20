7e61bb090c6b2f82e050d6d0552a47e460c477fd
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-08-20 15:07:10 -0600

    Refactor organization creation modal for i18n and add Spanish translations to lang/es.json

--- resources/views/livewire/organizations/create.blade.php
+++ resources/views/livewire/organizations/create.blade.php
@@ -24,17 +24,17 @@ new class extends Component {
 <flux:modal name="create-organization" class="md:w-96">
     <form wire:submit="create" class="space-y-6">
         <div>
-            <flux:heading size="lg">Create Organization</flux:heading>
-            <flux:text class="mt-2">Enter a name for your new organization.</flux:text>
+            <flux:heading size="lg">{{ __('Create Organization') }}</flux:heading>
+            <flux:text class="mt-2">{{ __('Enter a name for your new organization.') }}</flux:text>
         </div>
-        <flux:input label="Organization Name" placeholder="Acme Inc" wire:model="name" />
+        <flux:input label="{{ __('Organization Name') }}" placeholder="{{ __('Acme Inc') }}" wire:model="name" />
 
         <div class="flex gap-2">
             <flux:spacer />
             <flux:modal.close>
-                <flux:button variant="ghost" type="button">Cancel</flux:button>
+                <flux:button variant="ghost" type="button">{{ __('Cancel') }}</flux:button>
             </flux:modal.close>
-            <flux:button type="submit" variant="primary">Create</flux:button>
+            <flux:button type="submit" variant="primary">{{ __('Create') }}</flux:button>
         </div>
     </form>
 </flux:modal>
