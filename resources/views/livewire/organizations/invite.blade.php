3cc3d1b8796b00c1521506392267c5cd04387dd4
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-08-21 12:56:06 -0600

    i18n and UI: add Spanish translations and limit max width for organization member invitation form; remove autofocus for accessibility and consistency

--- resources/views/livewire/organizations/invite.blade.php
+++ resources/views/livewire/organizations/invite.blade.php
@@ -41,6 +41,24 @@ new class extends Component {
     }
 }; ?>
 
-<div>
-    //
+<div class="space-y-6">
+    <flux:heading size="lg">{{ __('Invite Member') }}</flux:heading>
+
+    <form wire:submit="sendInvitation" class="max-w-lg space-y-4">
+        <flux:field>
+            <flux:label>{{ __('Email address') }}</flux:label>
+            <flux:input.group>
+                <flux:input
+                    type="email"
+                    wire:model="email"
+                    placeholder="{{ __('Enter member email') }}"
+                     icon="user"
+                />
+                <flux:button type="submit">
+                    {{ __('Send Invitation') }}
+                </flux:button>
+            </flux:input.group>
+            <flux:error name="email" />
+        </flux:field>
+    </form>
 </div>
