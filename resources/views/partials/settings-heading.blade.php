27b70e1a1ebc02aad43e41fcebcd438206047873
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-09-10 19:57:01 -0600

    tui: refactor settings heading to match organization settings style for visual consistency

--- resources/views/partials/settings-heading.blade.php
+++ resources/views/partials/settings-heading.blade.php
@@ -1,5 +1,8 @@
-<div class="relative mb-6 w-full">
-    <flux:heading size="xl" level="1">{{ __('Settings') }}</flux:heading>
-    <flux:subheading size="lg" class="mb-6">{{ __('Manage your profile and account settings') }}</flux:subheading>
-    <flux:separator variant="subtle" />
+<div class="mb-8">
+    <flux:heading size="lg">
+        {{ __('Settings') }}
+    </flux:heading>
+    <flux:text class="mt-1">
+        {{ __('Manage your profile and account settings') }}
+    </flux:text>
 </div>
